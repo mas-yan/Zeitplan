c686934bac24c913e18632ff34ac78146daba622
mas-yan <mrey5597@gmail.com>
2023-02-04 11:41:46 +0700

    add: Button Component

--- src/compoents/Button.jsx
+++ src/compoents/Button.jsx
@@ -1,13 +1,13 @@
-export default function Button({children, className, textCol}) {
-    const addClassName = className ? `${className}` : '';
-    const addTextCol = textCol ?`${textCol}` : 'text-white';
-    return (
-        <>
-            <a className={`bg-primary-500 font-sans border border-primary-500 text-lg font-semibold ${addClassName}`}>
-                <span className={`${addTextCol}`}>
-                    {children}
-                </span>
-            </a>
-        </>
-    )
+export default function Button({ children, className, textCol }) {
+  const addClassName = className ? `${className}` : "";
+  const addTextCol = textCol ? `${textCol}` : "text-white";
+  return (
+    <>
+      <a
+        className={`bg-primary-500 cursor-pointer font-sans border border-primary-500 text-lg font-semibold ${addClassName}`}
+      >
+        <span className={`${addTextCol}`}>{children}</span>
+      </a>
+    </>
+  );
 }
