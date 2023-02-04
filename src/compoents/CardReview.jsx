f5583a8a4ed45d93ee766d9e65f1ab8c241fc5b0
mas-yan <mrey5597@gmail.com>
2023-02-04 11:42:04 +0700

    add: Card Review Component

--- src/compoents/CardReview.jsx
+++ src/compoents/CardReview.jsx
@@ -1,17 +1,36 @@
 export default function CardReview(props) {
-    return(
-        <div className="bg-white shadow-xl w-[300px] md:w-[469px] p-[30px] shadow-primary-500/20 rounded-md">
-            <img src="star.png" style={{'width': '120px', 'height': '20px'}} alt="star" />
-            <p className="text-dark-500 font-poppins text-sm leading-[26px] font-normal my-5">
-            {props.comment}
-            </p>
-            <div className="flex">
-            <img src={props.image} className="rounded-full mr-5" style={{'width': '55px'}} alt="star" />
-            <div>
-                <p className="font-poppins text-base font-medium">{props.name}</p>
-                <p className="font-poppins text-xs leading-[18px] font-medium text-dark-500 opacity-[60%]">{props.scholl}</p>
-            </div>
-            </div>
+  const width = props.class
+    ? props.class
+    : "w-[300px] md:w-[469px] shadow-primary-500/20";
+  const clsComnt = props.clsComnt ? props.clsComnt : "h-[150px] lg:h-[70px]";
+  return (
+    <div
+      className={`bg-white relative shadow-xl h-fit p-[30px] rounded-md ${width}`}
+    >
+      <img
+        src="star.png"
+        style={{ width: "120px", height: "20px" }}
+        alt="star"
+      />
+      <p
+        className={`text-dark-500 items-center flex font-poppins text-sm leading-[26px] font-normal my-5 ${clsComnt}`}
+      >
+        {props.comment}
+      </p>
+      <div className="flex">
+        <img
+          src={props.image}
+          className="rounded-full mr-5"
+          style={{ width: "55px" }}
+          alt="star"
+        />
+        <div>
+          <p className="font-poppins text-base font-medium">{props.name}</p>
+          <p className="font-poppins text-xs leading-[18px] font-medium text-dark-500 opacity-[60%]">
+            {props.scholl}
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
