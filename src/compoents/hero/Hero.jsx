cc5b631a0135039053877591ec438263c8425062
mas-yan <mrey5597@gmail.com>
2023-02-04 11:43:34 +0700

    update: Hero Component

--- src/compoents/hero/Hero.jsx
+++ src/compoents/hero/Hero.jsx
@@ -3,10 +3,11 @@ import CarrdCarousel from './CardCarousel'
 
 export default function Hero({addH}) {
     return (
+      <section>
         <div className={`flex relative flex-col-reverse lg:flex-row mt-12 lg:mt-20 ${addH}`}>
           <div className="lg:w-1/2 ">
-            <p className=" items-center justify-center md:text-center lg:text-left font-patua font-normal text-4xl md:text-5xl md:w-12/12 lg:text-6xl lg:w-10/12 2xl:w-9/12 leading:[20px] md:leading-[60px] text-dark-500">Sebuah cara mudah mengatur jadwal kuliah kamu</p>
-            <div className='font-sans md:w-12/12 2xl:w-3/5 md:text-center lg:text-left mt-4 leading-[24px] md:leading-[34px] text-[0.85rem] md:font-normal md:text-lg text-dark-500 text-opacity-70'>
+            <p className="items-center justify-center md:text-center lg:text-left font-patua font-normal text-4xl md:text-5xl md:w-12/12 lg:w-9/12 2xl:w-7/12 leading:[20px] md:leading-[60px] text-dark-500">Sebuah cara mudah mengatur jadwal kuliah kamu</p>
+            <div className='font-sans md:w-12/12 2xl:w-3/5 md:text-center lg:text-left mt-4 leading-[24px] md:leading-[34px] text-[0.85rem] md:text-lg md:font-normal  text-dark-500 text-opacity-60'>
               <p>Datang kuliah terlambat karena ketiduran itu tidak masalah;  </p>
               <p>datang kuliah karena lupa jadwal itu sangatlah tidak keren.</p>
               <p>Aplikasi ini adalah sahabat kamu sekarang, brodie~</p>
@@ -23,5 +24,6 @@ export default function Hero({addH}) {
             <img src="group 11.png" alt="" />
           </div>
         </div>
+      </section>
     )
 }
